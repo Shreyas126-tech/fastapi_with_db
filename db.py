da22a76ca791a6cf329ce888c07710b7bfed55da
Shreyas126-tech <pshreyas191@gmail.com>
2026-02-07 12:24:53 +0530

    commit

--- db.py
+++ db.py
@@ -9,11 +9,11 @@ base=declarative_base()
 
 DATABASE_URL=os.getenv("DATABASE_URL")
 print("DATABASE_URL",DATABASE_URL)
-engine=create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
+engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 def get_db():
-    db=create_engine(DATABASE_URL)
+    db = SessionLocal()
     try:
         yield db
     finally:
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from db import base
 from sqlalchemy import Column, Integer, String
-class user(base):
+class User(base):
     __tablename__="users"
     id=Column(Integer,primary_key=True,index=True)
     name=Column(String,unique=True,index=True)
